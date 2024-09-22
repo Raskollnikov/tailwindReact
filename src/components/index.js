ec8f2a8ea2f6cfe7655725ca407feec77d602d91
Raskollnikov <bakhalaia56@gmail.com>
2024-09-22 20:43:40 +0400

    app completed

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import ShoeCard from "./ShoeCard";
+import Button from "./Button";
+import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
+export { ShoeCard, Button, PopularProductCard, ServiceCard, ReviewCard };
